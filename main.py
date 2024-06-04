87879711b7b0e5856a298c89b6155ed659f46b83
Zoran Kolega <kolegazoran@gmail.com>
2024-06-04 13:34:33 -0400

    reorganized paths

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import download
 import extract
 import dedup
-import subtypes
+import etl
 
 import json
+import os
 
 if __name__ == "__main__":
 
@@ -11,19 +12,43 @@ if __name__ == "__main__":
     with open("config/config.json") as config_file:
         config = json.load(config_file)
 
+    # paths
+    data_dir_path = "data"
+    download_dir_path = os.path.join(data_dir_path, "download")
+    metadata_dir_path = os.path.join(data_dir_path, "metadata")
+    metadata_unique_dir_path = os.path.join(data_dir_path, "metadata_unique")
+    message_subtypes_unique_file_path = os.path.join(data_dir_path, "unique_message_subtypes_with_examples.csv")
+    intervention_measures_dir_path = os.path.join(data_dir_path, "intervention_measures")
+    intervention_measures_file_path = os.path.join(data_dir_path, "intervention_measures.csv")
+    intervention_measures_unique_file_path = os.path.join(data_dir_path, "intervention_measures_unique.csv")
+
+
     print("Downloading dataset...")
-    pid = config['dataset']['persistent_id']
-    download.download_dataverse_dataset(pid)
+    # download.download_dataverse_dataset(config['dataset']['persistent_id'],
+    #                                     download_dir_path)
 
     print("Extracting metadata files...")
-    download_path = pid
-    metadata_path = f'{pid}_metadata'
-    extract.extract_metadata(download_path, metadata_path)
+    extract.extract_metadata(download_dir_path,
+                             metadata_dir_path)
 
     print("Deduplicating metadata...")
-    dedup_metadata_path = f'{metadata_path}_unique'
-    dedup.save_unique_files(metadata_path, dedup_metadata_path)
+    dedup.save_unique_files(metadata_dir_path,
+                            metadata_unique_dir_path)
 
     print("Writing unique message subtype to file...")
-    subtypes_file_path = 'unique_message_subtypes_with_examples.csv'
-    subtypes.write_subtypes_to_csv(dedup_metadata_path, subtypes_file_path)
+    etl.write_subtypes_to_csv(metadata_unique_dir_path,
+                              message_subtypes_unique_file_path)
+
+    print("Writing intervention measures CSVs...")
+    etl.extract_and_rename_csv_files(download_dir_path,
+                                     intervention_measures_dir_path)
+
+    print("Writing intervention measures content CSV...")
+    etl.write_intervention_measures_content(intervention_measures_dir_path,
+                                            intervention_measures_file_path)
+
+    print("Deduplicating intervention measures content...")
+    intervention_measures_content_unique_file_path = "intervention_measures_content_unique.csv"
+    etl.write_intervention_measures_content_unique(intervention_measures_dir_path,
+                                                   intervention_measures_unique_file_path)
+   
