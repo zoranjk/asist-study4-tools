a2dc24a383c8e3ef49488c2035c17a0907889a71
Zoran Kolega <kolegazoran@gmail.com>
2024-06-04 11:33:22 -0400

    added function to extract and rename intervention measures CSV files

--- extract.py
+++ extract.py
@@ -17,3 +17,33 @@ def extract_metadata(zip_folder_path, output_path):
                         target = open(os.path.join(output_path, os.path.basename(member)), "wb")
                         with source, target:
                             target.write(source.read())
+
+
+def extract_and_rename_csv_files(source_dir, destination_dir):
+    os.makedirs(destination_dir, exist_ok=True)
+
+    for file in tqdm(os.listdir(source_dir)):
+        if file.endswith('.zip'):
+            zip_file_path = os.path.join(source_dir, file)
+            folder_name = os.path.splitext(file)[0]
+
+            try:
+                # print(f"Processing zip file: {file}")
+
+                with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                    # Extract only 'intervention_measures.csv' if it exists in the zip
+                    if 'intervention_measures.csv' in zip_ref.namelist():
+                        zip_ref.extract('intervention_measures.csv', destination_dir)
+                        # print(f"Extracted intervention_measures.csv from {file} to {destination_dir}")
+
+                        extracted_file = 'intervention_measures.csv'
+                        new_file_name = f"{folder_name}_{extracted_file}"
+                        os.rename(os.path.join(destination_dir, extracted_file),
+                                  os.path.join(destination_dir, new_file_name))
+                        # print(f"Renamed {extracted_file} to {new_file_name}")
+                    else:
+                        pass
+                        # print(f"No 'intervention_measures.csv' found in {file}.")
+            except zipfile.BadZipFile as e:
+                # print(f"Failed to process {file} due to a zipfile error: {e}")
+                continue  # Continue to the next file
